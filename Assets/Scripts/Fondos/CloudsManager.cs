05492a6ffb09fb4bb60cca94b6e151319515ddc6
valentina-rojas <valentinarojas618@gmail.com>
2025-08-23 10:05:57 -0300

    animacion nubes

---
+++ Assets/Scripts/Fondos/CloudsManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudsManager : MonoBehaviour
+{
+    public float velocidadMovimiento = 0.5f; 
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+
+    }
+
+    void Update()
+    {
+        float nuevoPosX = Mathf.Repeat(Time.time * velocidadMovimiento, 30f); 
+        transform.position = startPosition + Vector3.right * nuevoPosX;
+    }
+   
+}
+   
